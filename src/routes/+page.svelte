9d92490ec4852ad365a6945a7bca983f78adffd3
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-12-16 14:32:55 +0100

    😵

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <header>
+<nav></nav>
 </header>
 
 <main>
@@ -12,22 +13,26 @@
     <img src="/src/public/jheyhas.jpg" alt="" >
     <img src="/src/public/stephenhaytalk.jpg" alt="" >
     <img src="/src/public/unatalk.jpg" alt="" > -->
-  <h1>10 years of CSS day</h1>
+  
   <div class="intro">
-    <p>A decade of geeking out over <span>CSS</span></p>
+  <h1>10 years of CSS day</h1>
+  <article>
+    <h2>A decade of geeking out over CSS</h2>
+    <p class="text">CSS Day is a focused, advanced CSS conference. Our attendees and speakers are a mix of CSS designers, developers, spec writers and browser vendors, who take pride in what they know and do.</p>
+      <div class="button">
+     <a href="/">go to confrences</a>
+    </div>
+  <article>
+
   </div>
 
   <section class="wrapper">
     {#each Object.entries(data.data) as [year, event]}
-      <div class="cssday {year}" style="border-color: {event.color.hex};">
-          <h2>{event.title}</h2>
-                     <div class="info">
-          <h3>Attendees: {event.attendees.count}</h3>
-                    <ul>
-                    {#each event.countries as country}
-                <li>{country} </li>
-            {/each}
-            </ul>
+      <div class="cssday {year}">
+         <div class="info">
+           <h2>{event.title}</h2>
+             <h3>Attendees: {event.date}</h3>
+             <p class="text"></p>
             </div>
           <div class="mc">
             {#each event.mc as mc}
@@ -36,7 +41,9 @@
               {/if}
             {/each}
           </div>
+          <div class="button">
 <a href={event.link}>go to website</a>
+</div>
       </div>
     {/each}
   </section>
